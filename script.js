2de1fbf0820227417c0e01c804c03dad96699e93
Samuel Guevara <samsjamin@outlook.com>
2020-07-09 17:48:45 -0400

    revert

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ var special = ["`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", 
 var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 var upperCase = [ "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
 var numbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9",  "0" ];
+var password = "";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
-  passwordText.value = password;
+var passwordCharacters;
+var allArrays = [];
 
+function generate() {
+    
 }
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
