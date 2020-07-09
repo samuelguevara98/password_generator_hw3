69a9583d077fe4692955e26d90f866a0d0894ad6
Samuel Guevara <samsjamin@outlook.com>
2020-07-09 17:51:18 -0400

    Revert "revert"
    This reverts commit 2de1fbf0820227417c0e01c804c03dad96699e93.

--- script.js
+++ script.js
@@ -5,19 +5,16 @@ var special = ["`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", 
 var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 var upperCase = [ "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
 var numbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9",  "0" ];
-var password = "";
 
-var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-var passwordCharacters;
-var allArrays = [];
+  passwordText.value = password;
 
-function generate() {
-    
 }
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
